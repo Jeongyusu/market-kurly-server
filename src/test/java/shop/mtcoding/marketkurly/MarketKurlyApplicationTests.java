1f3e9e717d329ddc714e850da9d9012eac255038
MMGGHD <tldmseogus@naver.com>
2023-10-25 10:20:49 +0900

    [김대현] 기본세팅 수정

--- src/test/java/shop/mtcoding/marketkurly/MarketKurlyApplicationTests.java
+++ src/test/java/shop/mtcoding/marketkurly/MarketKurlyApplicationTests.java
@@ -2,12 +2,3 @@ package shop.mtcoding.marketkurly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MarketKurlyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
