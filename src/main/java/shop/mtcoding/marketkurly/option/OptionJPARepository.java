2182e64e9063104c1d6cb4bf4118289f712c3d77
KimHaYu <z2poter@naver.com>
2023-10-28 00:48:28 +0900

    [김하율]상품화면 컬리추천, 금주혜택 및 유저 코드 리펙토리

---
+++ src/main/java/shop/mtcoding/marketkurly/option/OptionJPARepository.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.marketkurly.option;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import shop.mtcoding.marketkurly.product.Product;
+
+public interface OptionJPARepository extends JpaRepository<Option, Integer> {
+    Optional<Option> findTopByProductOrderByOptionPriceAsc(Product product);
+
+}
