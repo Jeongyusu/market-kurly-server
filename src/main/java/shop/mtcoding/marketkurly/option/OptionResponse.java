c4b450482241c17c3c97a42b98e25fe97ea712c8
MMGGHD <tldmseogus@naver.com>
2023-11-01 02:03:38 +0900

    [김대현] optionDTO, productQuestionDTO수정

--- src/main/java/shop/mtcoding/marketkurly/option/OptionResponse.java
+++ src/main/java/shop/mtcoding/marketkurly/option/OptionResponse.java
@@ -13,6 +13,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -21,10 +22,43 @@ import javax.persistence.ManyToOne;
 import org.hibernate.annotations.CreationTimestamp;
 
 public class OptionResponse {
+
     @ToString
     @Getter
     @NoArgsConstructor
     public static class OptionSelectMainDTO {
-    }
 
+        private Integer productId;
+        private String productSellerName;
+        private Integer productSellerId;
+        private String productName;
+        private List<OptionMainDTO> optionMainDTOs;
+
+        public OptionSelectMainDTO(List<Option> options) {
+            this.productId = options.get(0).getProduct().getId();
+            this.productSellerId = options.get(0).getProduct().getSeller().getId();
+            this.productSellerName = options.get(0).getProduct().getSeller().getUsername();
+            this.optionMainDTOs = options.stream()
+                    .map(option -> new OptionMainDTO(productSellerName, option))
+                    .collect(Collectors.toList());
+        }
+
+        @ToString
+        @Getter
+        @NoArgsConstructor
+        public class OptionMainDTO {
+
+            private Integer optionId;
+            private String productSellerName;
+            private String optionName;
+            private Integer optionPrice;
+
+            public OptionMainDTO(String productSellerName, Option option) {
+                this.optionId = option.getId();
+                this.productSellerName = productSellerName;
+                this.optionName = option.getOptionName();
+                this.optionPrice = option.getOptionPrice();
+            }
+        }
+    }
 }
--- src/main/java/shop/mtcoding/marketkurly/productquestion/ProductQuestionJPARepository.java
+++ src/main/java/shop/mtcoding/marketkurly/productquestion/ProductQuestionJPARepository.java
@@ -3,6 +3,6 @@ package shop.mtcoding.marketkurly.productquestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface ProdcutQuestionJPARepository extends JpaRepository<ProductQuestion, Integer> {
+public interface ProductQuestionJPARepository extends JpaRepository<ProductQuestion, Integer> {
     List<ProductQuestion> findByProductId(Integer productId);
 }
