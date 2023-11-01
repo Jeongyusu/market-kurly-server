5b039d142dabf458e945d5aeef766b089a376bb7
MMGGHD <tldmseogus@naver.com>
2023-11-01 12:42:22 +0900

    [김대현] coupon테이블 할인율, 무료배송여부 삭제

--- src/main/java/shop/mtcoding/marketkurly/product/Product.java
+++ src/main/java/shop/mtcoding/marketkurly/product/Product.java
@@ -39,10 +39,6 @@ public class Product {
     @ManyToOne(fetch = FetchType.LAZY)
     private User seller; // 판매자
 
-    // 관리자에의해서 판매승인 되었는가? << 기본값 false
-    @Nullable
-    private Boolean isAccepted = false;
-
     private LocalDate productUploadedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -57,7 +53,6 @@ public class Product {
             String productContent,
             LocalDate productUploadedAt,
             Category category,
-            Boolean isAccepted,
             User user,
             LocalDate discountExpiredAt) {
         this.id = id;
@@ -68,7 +63,6 @@ public class Product {
         this.discountRate = discountRate;
         this.productUploadedAt = productUploadedAt;
         this.discountExpiredAt = discountExpiredAt;
-        this.isAccepted = isAccepted;
         this.category = category;
         this.seller = user;
     }
