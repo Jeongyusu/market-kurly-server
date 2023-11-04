6d2c262a186ee64793b8c988a130aec1c9b3e8c5
MMGGHD <tldmseogus@naver.com>
2023-11-05 04:29:26 +0900

    [김대현] 주석제거, 약간 수정

--- src/main/java/shop/mtcoding/marketkurly/testController.java
+++ src/main/java/shop/mtcoding/marketkurly/testController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -115,6 +116,7 @@ public class testController {
     }
 
     @PostMapping("/product/save")
+    @ResponseBody
     public ResponseEntity<?> saveTest(
             @RequestParam("productThumbnail") MultipartFile productThumbnail,
             @RequestParam("productDetailPic") MultipartFile productDetailPic,
